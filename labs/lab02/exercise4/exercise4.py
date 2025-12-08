ac1f3790066937644712d8f91f185ea57fba9da7
PutriNoratira <ptrnrtrsyaa@gmail.com>
2025-12-08 12:25:53 +0800

    update

--- labs/lab02/exercise4/exercise4.py
+++ labs/lab02/exercise4/exercise4.py
@@ -10,17 +10,17 @@ def get_hourly_rate(vehicle_type, hour_24):
     vehicle_type = vehicle_type.lower()
 
     if vehicle_type == "electric":
-        rate = low_rate * hour_24
-        return rate
+        rate = low_rate
+        return low_rate
     elif vehicle_type == "hybrid":
         if hour_24 <= 6 or hour_24 >= 22:
-            rate = low_rate * hour_24
+            rate = low_rate
             return rate
         else:
-            rate = high_rate * hour_24
+            rate = high_rate
             return rate
     else:
-        rate = high_rate * hour_24
+        rate = high_rate
         return rate
 
 # Test your code here
