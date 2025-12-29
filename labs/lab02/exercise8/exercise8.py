263877e9078377fd5e7044f1971a0fec0bd6a46c
PutriNoratira <ptrnrtrsyaa@gmail.com>
2025-12-29 11:36:56 +0800

    update

--- labs/lab02/exercise8/exercise8.py
+++ labs/lab02/exercise8/exercise8.py
@@ -2,29 +2,31 @@ def calculate_bounce_height(current_height):
     """
     Calculate the next bounce height (80% of current).
     """
-    # TODO: Implement this
-    pass
-
+    height = current_height * 0.8
+    return height
 
 def is_ball_stopped(height):
     """
     Check if the ball has stopped (height < 1).
     """
-    # TODO: Implement this
-    pass
-
+    return height < 1
 
 def calculate_bounce_count(initial_height):
     """
     Count how many times the ball bounces.
     """
-    # TODO: Implement this
-    pass
+    count = 0
+    current_height = initial_height
 
+    while True:
+        current_height = calculate_bounce_height(current_height)
+        if is_ball_stopped(current_height):
+            break
+         
+    
 
 def calculate_total_distance(initial_height):
     """
     Calculate total distance traveled.
     """
-    # TODO: Implement this
-    pass
+    
