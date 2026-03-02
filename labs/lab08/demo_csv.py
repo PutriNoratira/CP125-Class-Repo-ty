bf414510dff968f7a2d32afa1105d04e0bc4c2d4
PutriNoratira <ptrnrtrsyaa@gmail.com>
2026-03-02 12:25:33 +0800

    update

--- labs/lab08/demo_csv.py
+++ labs/lab08/demo_csv.py
@@ -2,18 +2,28 @@
 import csv
 
 # Read CSV file
-print("=== Reading CSV ===")
-f = open("data/students.csv", "r", newline="")
+'''print("=== Reading CSV ===")
+f = open("labs/lab08/data/students.csv", "r", newline="")
 reader = csv.reader(f)
 
 for row in reader:
     print(row)
 
+f.close()'''
+
+'''print("=== Reading CSV ===")
+f = open("labs/lab08/data/students.csv", "r", newline="")
+reader = csv.reader(f)
+content = []
+for row in reader:
+    content.append(row)
+
 f.close()
+print(content)'''
 
 # Write CSV file
-print("\n=== Writing CSV ===")
-f = open("data/output.csv", "w", newline="")
+'''print("\n=== Writing CSV ===")
+f = open("labs/lab08/data/output.csv", "w", newline="")
 writer = csv.writer(f)
 
 # Write header
@@ -26,3 +36,31 @@ writer.writerow(["Ahmad", 78, "B"])
 
 f.close()
 print("CSV written to data/output.csv")
+
+print("\n=== Writing CSV ===")
+f = open("labs/lab08/data/output.csv", "w", newline="")
+writer = csv.writer(f)
+
+# Write header
+writer.writerow(["Name", "Score"])
+
+# Write data
+writer.writerow(["Ali", 85])
+writer.writerow(["Sara", 92])
+writer.writerow(["Ahmad", 78])
+
+f.close()'''
+
+import csv
+
+records = [
+    ["Name", "Score"],
+    ["Ali", 85],
+    ["Sara", 92],
+    ["Ahmad", 78]
+]
+
+f = open("labs/lab08/data/output.csv", "w", newline="")
+writer = csv.writer(f)
+writer.writerows(records)
+f.close()
